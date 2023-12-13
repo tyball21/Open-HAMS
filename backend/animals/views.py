3a8484f4fdea7b4d9d039fc0fa89ab4a59169bac
tyball21 <tylerdiamse@gmail.com>
2023-12-13 15:48:58 -0700

    made /animals/views.py more commprehensive. added init packages to main folders like /backend

--- backend/animals/views.py
+++ backend/animals/views.py
@@ -1,43 +1,81 @@
 # backend/animals/views.py
+# Importing the blueprint
 from . import animals_blueprint
+
+# Importing necessary Flask and SQLAlchemy components
 from flask.views import MethodView
-from .models import Animal  # assuming you have an Animal model in models.py
-from .schemas import AnimalSchema  # assuming you have an AnimalSchema in schemas.py
+from sqlalchemy.exc import SQLAlchemyError
+
+# Importing your models and schemas
+from .models import Animal  # Import the Animal model
+from .schemas import AnimalSchema  # Import the AnimalSchema for serialization
+
+# Import the database instance (db) directly
+from backend.app import db
 
 
 @animals_blueprint.route('/')
 class Animals(MethodView):
     @animals_blueprint.response(200, AnimalSchema(many=True))
     def get(self):
-        """List all animals"""
-        animals = Animal.query.all()  # or however you get all animals
+        """List all animals with pagination"""
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 10, type=int)  # default to 10 items per page
+        paginated_animals = Animal.query.paginate(page, per_page, error_out=False)
+        animals = paginated_animals.items
         return animals
+
 
     @animals_blueprint.arguments(AnimalSchema)
     @animals_blueprint.response(201, AnimalSchema)
     def post(self, new_data):
         """Add a new animal"""
         animal = Animal(**new_data)
-        # add code to save the new animal to the database
+        try:
+            db.session.add(animal)  # Add the new animal to the session
+            db.session.commit()  # Commit the session to save the animal
+        except SQLAlchemyError as e:
+            db.session.rollback()  # Rollback the transaction in case of error
+            return {"message": str(e)}, 400  # Return an error message and a 400 status code
         return animal
+
 
 @animals_blueprint.route('/<int:animal_id>')
 class AnimalById(MethodView):
     @animals_blueprint.response(200, AnimalSchema)
     def get(self, animal_id):
         """Get animal by ID"""
-        animal = Animal.query.get(animal_id)  # or however you get an animal by ID
+        animal = Animal.query.get(animal_id)  # Retrieve an animal by ID
         return animal
 
     @animals_blueprint.arguments(AnimalSchema)
     @animals_blueprint.response(200, AnimalSchema)
     def put(self, update_data, animal_id):
         """Update an existing animal"""
-        animal = Animal.query.get(animal_id)  # or however you get an animal by ID
-        # add code to update the animal in the database
+        animal = Animal.query.get(animal_id)  # Retrieve an animal by ID
+        if not animal:
+            return {"message": "Animal not found"}, 404
+        try:
+            for key, value in update_data.items():
+                if key not in ['id', 'created_at']:  # Exclude fields that shouldn't be updated
+                    setattr(animal, key, value)
+            db.session.commit()  # Commit the session to save the changes
+        except SQLAlchemyError as e:
+            db.session.rollback()  # Rollback the transaction in case of error
+            return {"message": str(e)}, 400  # Return an error message and a 400 status code
         return animal
+
 
     @animals_blueprint.response(204)
     def delete(self, animal_id):
         """Delete an animal"""
-        # add code to delete the animal from the database
+        animal = Animal.query.get(animal_id)  # Retrieve an animal by ID
+        if not animal:
+            return {"message": "Animal not found"}, 404
+        try:
+            db.session.delete(animal)  # Delete the animal from the session
+            db.session.commit()  # Commit the session to delete the animal
+        except SQLAlchemyError as e:
+            db.session.rollback()  # Rollback the transaction in case of error
+            return {"message": str(e)}, 400  # Return an error message and a 400 status code
+        return '', 204  # Return an empty response with a 204 status code
--- backend/schemas/animal_schema.py
+++ backend/schemas/animal_schema.py
@@ -12,18 +12,20 @@ class AnimalSchema(SQLAlchemyAutoSchema):
     kind = auto_field()
     species = auto_field()
     image = auto_field()
-    max_daily_checkouts = auto_field()
-    max_checkout_hours = auto_field()
-    rest_time = auto_field()
+    max_daily_checkouts = auto_field()  # only set by admin & mod for animal saftey
+    max_checkout_hours = auto_field()   # only set by admin & mod for animal saftey
+    rest_time = auto_field()        # set by admin/mod will be the mandatory time to rest after an event
     description = auto_field()
     checked_in = auto_field()
-    handling_enabled = auto_field()
-    created_at = auto_field()
-    updated_at = auto_field()
+    handling_enabled = auto_field()    # admin/mod only for special situations so noone can checkout the animal
+    created_at = auto_field(dump_only=True)
+    updated_at = auto_field(dump_only=True)
     zoo_id = auto_field()
 
     # Optional: Nested relationships, uncomment if needed
+        # regarding animal_events, it may be good to see if an animal event is true and click to view the event  
     # animal_events = ma.Nested('AnimalEventSchema', many=True, exclude=('animal',))
+        # regarding animal_comments each animal should have it's only profile page and view comments/log there. same goes for the relationships below here 
     # animal_comments = ma.Nested('AnimalCommentSchema', many=True, exclude=('animal',))
     # audits = ma.Nested('AnimalAuditSchema', many=True, exclude=('animal',))
     # health_logs = ma.Nested('AnimalHealthLogSchema', many=True, exclude=('animal',))
