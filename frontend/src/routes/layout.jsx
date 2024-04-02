8b923313c268c2f1d743f6f3248086da17babf98
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-04-02 22:39:01 +0500

    fix sidebar

--- frontend/src/routes/layout.jsx
+++ frontend/src/routes/layout.jsx
@@ -6,10 +6,10 @@ export function RootLayout() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <body className="bg-body mx-auto flex min-h-screen bg-blueish">
-        <div className="hidden h-full w-56 lg:block">
+        <div className="fixed left-0 top-0 hidden h-full w-56 lg:block">
           <Sidebar />
         </div>
-        <main className="flex flex-1 flex-col overflow-y-auto">
+        <main className="flex flex-1 flex-col overflow-y-auto lg:ml-56">
           <div className="px-4 py-4 lg:px-8">
             <Outlet />
           </div>
