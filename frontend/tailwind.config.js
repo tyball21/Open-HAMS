6bd8a674ab9f7648511d57fe6b615b3d32084c95
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-04-02 14:38:12 +0500

    Update foreground color in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,7 +26,7 @@ export default {
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        foreground: "var(--foreground)",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
