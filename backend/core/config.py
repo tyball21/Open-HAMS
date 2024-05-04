db9d06c23c7a0fe071069325b1d0c077ad87eb11
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-04 12:19:53 +0500

    add app name and version

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,9 +4,14 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 
 load_dotenv()
+print(os.getenv("DB_URI"))
 
 
 class Config(BaseModel):
+    app_name: str = "Open HAMS"
+    app_description: str = "Open Source Habitat Animal Management System - A SaaS dashboard for Zoo's and ambassador programs for animal and event management. Designed for any organization to host and run their own animal management dashboard."
+    app_version: str = "0.1.0"
+
     db_uri: str = os.getenv("DB_URI")
 
 
