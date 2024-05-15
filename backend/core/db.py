9a76081a15b5f7ee7986c53c3a49c567b7288fc4
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-15 15:54:46 +0500

    refactor: Update db.py to use settings instead of config for database URI

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from core.config import config
+from core.config import settings
 
-engine = create_async_engine(config.db_uri, echo=True)
+engine = create_async_engine(settings.DB_URI, echo=True)
