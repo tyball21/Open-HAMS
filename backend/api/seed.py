7aa15f34fabc708c2a542c8c0001da5ad85ece44
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-15 22:22:11 +0500

    reconfigure permissions

--- backend/api/seed.py
+++ backend/api/seed.py
@@ -1,40 +1,55 @@
 from core.config import settings
 from core.db import engine
 from core.security import get_password_hash
+from db.roles import get_role
 from db.users import get_user_by_email
-from models import Permission, Role, User
+from db.zoo import get_main_zoo, get_zoo_by_name
+from models import Permission, Role, User, Zoo
 from sqlalchemy.exc import IntegrityError
-from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 
 async def seed_roles_and_permissions() -> None:
     async with AsyncSession(engine) as session:
-        permissions = [
-            Permission(name="manage_animals"),
-            Permission(name="manage_events"),
-            Permission(name="manage_users"),
-            Permission(name="edit_zoo_settings"),
-            Permission(name="create_events"),
-            Permission(name="checkout_animals"),
-            Permission(name="view_animals"),
-            Permission(name="view_events"),
-            # ... other permissions as necessary ...
+        permissions = {
+            "manage_animals": Permission(name="manage_animals"),
+            "manage_events": Permission(name="manage_events"),
+            "manage_users": Permission(name="manage_users"),
+            "edit_zoo_settings": Permission(name="edit_zoo_settings"),
+            "create_events": Permission(name="create_events"),
+            "checkout_animals": Permission(name="checkout_animals"),
+            "view_animals": Permission(name="view_animals"),
+            "view_events": Permission(name="view_events"),
+        }
+
+        admin_permissions = [key for key in permissions.keys()]  # all permissions
+        moderator_permissions = [
+            "view_animals",
+            "view_events",
+            "manage_animals",
+            "manage_events",
         ]
 
-        admin_permissions = permissions
-        moderator_permissions = permissions[:2]  # manage_animals, manage_events
+        handler_permissions = [
+            "view_animals",
+            "view_events",
+            "checkout_animals",
+            "create_events",
+        ]
 
-        # view_animals, view_events, checkout_animals, create_events
-        basic_permissions = permissions[4:]
+        visitors_permissions = ["view_animals", "view_events"]
+
+        def get_permissions(permission_list: list[str]):
+            return [permissions[permission] for permission in permission_list]
 
         roles = [
-            Role(name="admin", permissions=admin_permissions),
-            Role(name="moderator", permissions=moderator_permissions),
-            Role(name="basic", permissions=basic_permissions),
+            Role(name="admin", permissions=get_permissions(admin_permissions)),
+            Role(name="moderator", permissions=get_permissions(moderator_permissions)),
+            Role(name="handler", permissions=get_permissions(handler_permissions)),
+            Role(name="visitor", permissions=get_permissions(visitors_permissions)),
         ]
 
-        for permission in permissions:
+        for permission in permissions.values():
             try:
                 session.add(permission)
                 await session.commit()
@@ -69,8 +84,25 @@ async def create_admin() -> None:
             last_name="User",
             username=settings.ADMIN_USERNAME,
             hashed_password=get_password_hash(settings.ADMIN_PASSWORD),
-            role=(await session.exec(select(Role).where(Role.name == "admin"))).first(),
+            role=await get_role("admin", session),
+            zoo=await get_main_zoo(session),
         )
 
         session.add(user)
+        await session.commit()
+
+
+async def create_zoo() -> None:
+    async with AsyncSession(engine) as session:
+        zoo_exists = await get_zoo_by_name(settings.ZOO_NAME, session)
+
+        if zoo_exists:
+            return
+
+        zoo = Zoo(
+            name=settings.ZOO_NAME,
+            location=settings.ZOO_LOCATION,
+        )
+
+        session.add(zoo)
         await session.commit()
