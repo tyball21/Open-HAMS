d20a5ef6b0335c86038c02aee7cb1a77a60ce8b9
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-15 15:53:50 +0500

    refactor: Update __init__.py to use APIRouter for routing

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,1 +1,7 @@
-from .routes.users import router as users_router # noqa
+from fastapi import APIRouter
+
+from .routes.users import router as users_router
+
+api_router = APIRouter()
+
+api_router.include_router(users_router)
