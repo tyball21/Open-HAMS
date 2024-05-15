4532a9b1016fed0da0f0e2da8bad2be4aedd8682
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-15 15:55:19 +0500

    refactor: Update app.py to use asynccontextmanager for lifespan

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,27 @@
+from contextlib import asynccontextmanager
+
+from api import api_router
+from api.seed import create_admin, seed_db
+from core.config import settings
 from fastapi import FastAPI
 
-from api import users_router
-from core.config import settings
 
-app = FastAPI(title=settings.app_name, version=settings.app_version)
-
-
-@app.get("/")
-def hello():
-    return {"Hello": "World"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await seed_db()
+    await create_admin()
+    yield
 
 
-app.include_router(users_router)
+app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION, lifespan=lifespan)
+
+
+app.include_router(api_router)
+
+# @app.get("/docs", include_in_schema=False)
+# async def custom_swagger_ui_html_cdn():
+#     return get_swagger_ui_html(
+#     openapi_url=app.openapi_url,
+#     title=f"{app.title} - Swagger UI",
+#     swagger_css_url="https://cdn.jsdelivr.net/gh/Itz-fork/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css"
+# )
