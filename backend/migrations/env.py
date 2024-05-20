1823f9eca5b210d0a67710017e59219fb49e6f51
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-20 22:09:59 +0500

    fix env vars for alembic migrations

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,5 +1,7 @@
+import os
 import asyncio
 from logging.config import fileConfig
+from dotenv import load_dotenv
 
 from alembic import context
 from sqlalchemy import pool
@@ -7,11 +9,16 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel  # NEW
 
+load_dotenv()
+
 from models import *  # noqa
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option('sqlalchemy.url', os.getenv('DB_URI'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
