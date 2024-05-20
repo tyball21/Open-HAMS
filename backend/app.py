1a44c4705ae8e163cc04cc05cd7466e9e6d43e77
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-05-20 21:52:15 +0500

    refactor: Update app.py to include CORS middleware

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,49 @@
 from contextlib import asynccontextmanager
 
 from api import api_router
-from api.seed import create_admin, seed_db
+from api.deps import SessionDep
+from api.seed import create_admin, create_zoo, seed_db
 from core.config import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from models import Permission, Role, RolePermission, User
+from sqlmodel import select
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await seed_db()
+    await create_zoo()
     await create_admin()
     yield
 
 
+# Add cors
+
 app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION, lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # will be changed to the frontend url
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
-# @app.get("/docs", include_in_schema=False)
-# async def custom_swagger_ui_html_cdn():
-#     return get_swagger_ui_html(
-#     openapi_url=app.openapi_url,
-#     title=f"{app.title} - Swagger UI",
-#     swagger_css_url="https://cdn.jsdelivr.net/gh/Itz-fork/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css"
-# )
+
+# For debugging purposes
+@app.get("/clear")
+async def clear(session: SessionDep):
+    async def truncate_table(model):
+        objs = await session.exec(select(model))
+        for obj in objs:
+            await session.delete(obj)
+        await session.commit()
+
+    models = [User, RolePermission, Permission, Role]
+    for model in models:
+        await truncate_table(model)
+
+    return {"message": "Tables cleared"}
